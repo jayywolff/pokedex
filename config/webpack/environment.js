2729eea60baa5861a214597b559902252f6c625b
Jayy Wolff <jaywolff16@gmail.com>
2019-08-27 22:35:21 -0400

    webpack config

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,8 @@
 const { environment } = require('@rails/webpacker')
+const customConfig = require('./custom')
 const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loaders/vue')
-
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
+environment.config.merge(customConfig)
 module.exports = environment
